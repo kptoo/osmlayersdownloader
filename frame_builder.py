1c36cedee9977b8851d4aec1aaec603e0522cebe
kptoo <88309319+kptoo@users.noreply.github.com>
2026-02-28 16:18:13 +0300

    Add FrameBuilder class for creating frame geometry

---
+++ frame_builder.py
@@ -0,0 +1,8 @@
+class FrameBuilder:
+    def __init__(self):
+        pass
+
+    def create_frame_geometry(self, width, height):
+        """Create a frame geometry with the specified width and height."""
+        # Frame geometry creation logic here
+        return {'width': width, 'height': height}
